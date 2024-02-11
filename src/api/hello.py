ba52dfaa4af2c793f1e576c2ddeb0084e853c584
Azfar41268 <azfarsikander33@gmail.com>
2024-02-11 10:48:56 +0500

    okk

--- src/api/hello.py
+++ src/api/hello.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine.base import Engine
 
-app = FastAPI()
+db_url = "postgresql://azfarsikander33:t2mwbLdBS9RE@ep-withered-bar-a5adq13x.us-east-2.aws.neon.tech/todos?sslmode=require"
+
+engine: Engine = create_engine(db_url)
+
+
+app: FastAPI = FastAPI()
 
 @app.get("/api/python")
 def hello():
-    return "Hello Wrold"
+    with engine.connect() as conn:
+        todo = conn.execute(text("create table if not exists todo (id serial primary key, title varchar(255), completed boolean)"))
+        todoInput = conn.execute(text("insert into todo (title, completed) values ('Learn Python', True)"))
+        result = conn.execute(text("select * from todo"))
+        print(result.all())
+        a = result.all()
+        print(len(a))
+        return {"msg": a}
+
